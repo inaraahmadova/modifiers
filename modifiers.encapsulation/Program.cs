cdffd57ff46d702f336fbf41c294e9b2d4082347
inaraahmadova <tu7r5hs82@code.edu.az>
2024-04-02 18:58:33 +0400

    first commit

---
+++ modifiers.encapsulation/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace modifiers.encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon(10, 1,2, true);
+
+            while (true)
+            {
+                Console.WriteLine("emeliyyati secin: ");
+                Console.WriteLine("0 - İnformasiya almaq ücün");
+                Console.WriteLine("1 - Shoot metodu ucun");
+                Console.WriteLine("2 - Fire metodu ucun");
+                Console.WriteLine("3 - GetRemainBulletCount metodu ucun");
+                Console.WriteLine("4 - Reload metodu ucun");
+                Console.WriteLine("5 - ChangeFireMode metodu ucun");
+                Console.WriteLine("6 - Proqramdan dayandırmaq ucun");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "0":
+                        Console.WriteLine($"BulletCapacity: {weapon.BulletCapacity}");
+                        Console.WriteLine($"BulletCount: {weapon.BulletCount}");
+                        Console.WriteLine($"DischargeTime: {weapon.DischargeTime}");
+                        Console.WriteLine($"ShootingMode: {(weapon.ShootingMode ? "avtomatik" : "tekli")}");
+                        break;
+                    case "1":
+                        weapon.Shoot();
+                        break;
+                    case "2":
+                        weapon.Fire();
+                        break;
+                    case "3":
+                        Console.WriteLine($"Qalan gulle sayi: {weapon.GetRemainBulletCount()}");
+                        break;
+                    case "4":
+                        weapon.Reload();
+                        break;
+                    case "5":
+                        weapon.ChangeFireMode();
+                        break;
+                    case "6":
+                        return; 
+                }
+            }
+        }
+    }
+}
---
+++ modifiers.encapsulation/Weapon.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modifiers.encapsulation
+{
+    internal class Weapon
+    {
+        public int BulletCapacity; 
+        public int BulletCount; 
+        public int DischargeTime; 
+        public bool ShootingMode;
+        public Weapon(int bulletCapacity, int bulletCount, int dischargeTime, bool shootingMode)
+        {
+            BulletCapacity = bulletCapacity;
+            BulletCount = bulletCapacity; 
+            DischargeTime = dischargeTime;
+            ShootingMode = shootingMode;
+        }
+
+        public void Shoot()
+        {
+            if (BulletCount>0)
+            {
+                Console.WriteLine("Gulle atildi.");
+                BulletCount--;
+            }
+            else
+            {
+                Console.WriteLine("Daraqda gulle qalmayib.");
+            }
+        }
+        
+
+
+        public void Fire()
+        {
+            int remainBullets = BulletCount;
+            int timeFinish = remainBullets * DischargeTime;
+            Console.WriteLine($"Hamisi ateslendi. {timeFinish} saniyeye bitdi.");
+        }
+
+
+
+
+        public int GetRemainBulletCount()
+        {
+            return BulletCapacity - BulletCount;
+        }
+
+
+
+        public void Reload()
+        {
+            BulletCount = BulletCapacity; 
+            Console.WriteLine("Daraq yeniden dolduruldu.");
+        }
+
+
+
+
+        public void ChangeFireMode()
+        {
+            ShootingMode = !ShootingMode;
+        }
+
+
+
+    }
+}
